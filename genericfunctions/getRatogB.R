f0e39e2714915a57356adaffe22e1e867ba30669
dvchene <dvchene@dvchene.local>
2015-02-01 07:46:40 +1100

    Added function to extract rate trees from beast2 trees output.

---
+++ genericfunctions/getRatogB.R
@@ -0,0 +1,13 @@
+# This function is to get ratograms from the output of beast2. It takes an input file, and returns an R object or trees.
+
+require(phangorn)
+
+getRatogB <- function(trees, out.file = "ratogs.nex"){
+
+	  trs <- readLines(trees)
+
+	  trs <- gsub("[[]&([a-z])+[=]", ":", trs)
+	  trs <- gsub("[]]:([0-9]|[.])+", "", trs)
+
+	  writeLines(trs, out.file)
+}
